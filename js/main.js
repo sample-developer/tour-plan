1642d2dc121cbd7eef9b905a79fb5327ded988c7
sample-developer <bergomot85@gmail.com>
2020-07-20 23:56:19 +0300

    create: keyboard swiper-slider control

--- js/main.js
+++ js/main.js
@@ -7,4 +7,9 @@ var mySwiper = new Swiper('.swiper-container', {
     nextEl: '.slider-button--next',
     prevEl: '.slider-button--prev',
   },
+
+  keyboard: {
+    enable: true,
+    pageUpDown: false,
+  }
 })
