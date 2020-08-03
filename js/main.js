d02931617b34fd39480b98e3b37950c75b5802cf
sample-developer <bergomot85@gmail.com>
2020-08-04 00:43:10 +0300

    create: added function to close modal with escape

--- js/main.js
+++ js/main.js
@@ -42,7 +42,8 @@ $(document).ready(function () {
   var closeModalButton = $('.modal__close');
 
   modalButton.on('click', openModal);
-  closeModalButton.on('click', closeModal)
+  closeModalButton.on('click', closeModal);
+
 
   function openModal() {
     var targetModal = $(this).attr("data-href");
@@ -57,5 +58,14 @@ $(document).ready(function () {
     modalOverlay.removeClass('modal__overlay--visible')
     modalDialog.removeClass('modal__dialog--visible')
   };
+
+
+  $(document).on('keydown', function (e) {
+    if (e.keyCode == 27)
+      var modalOverlay = $('.modal__overlay');
+    var modalDialog = $('.modal__dialog');
+    modalOverlay.removeClass('modal__overlay--visible');
+    modalDialog.removeClass('modal__dialog--visible');
+  });
 
 });
